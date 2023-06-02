6d91c5f4e3364685a7fa366a899e0256f76bdae1
Chris Chan <chanchrisdominic@gmail.com>
2023-06-03 00:44:00 +0800

    Added logged in user

--- ThaiTanic/Forms/frmDashboard.cs
+++ ThaiTanic/Forms/frmDashboard.cs
@@ -7,25 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ThaiTanic.Entities;
+using ThaiTanic.State;
 
 namespace ThaiTanic.Forms
 {
     public partial class frmDashboard : Form
     {
-        public frmDashboard()
+        private readonly User loggedInUser;
+        private readonly Cart cart;
+
+        public frmDashboard(User user)
         {
             InitializeComponent();
 
-            frmMenu frmMenu = new frmMenu();
-            frmMenu.TopLevel = false;
-            frmMenu.Dock = DockStyle.Fill;
+            loggedInUser = user;
+            cart = new Cart(user);
+            frmMenu frmMenu = new frmMenu(AddCartEntry)
+            {
+                TopLevel = false,
+                Dock = DockStyle.Fill
+            };
             pnlContainer.Controls.Add(frmMenu);
             frmMenu.Show();
         }
 
+        private void AddCartEntry(Items item, int quantity)
+        {
+            cart.AddCartEntry(item.Id, quantity);
+        }
+
         private void guna2ImageButton1_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
     }
 }
