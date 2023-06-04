a33f01da48df279b97475fb0f7ccbe5a80e427ce
Rvondc <rogivon.delacruz@gmail.com>
2023-06-04 18:38:23 +0800

    feat: Add category card user control

---
+++ ThaiTanic/UserControls/ucCategoryCard.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace ThaiTanic.UserControls
+{
+    partial class ucCategoryCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCategoryName = new System.Windows.Forms.Label();
+            this.pnlBackground = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlBackground.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCategoryName
+            // 
+            this.lblCategoryName.AutoSize = true;
+            this.lblCategoryName.Font = new System.Drawing.Font("Corbel", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoryName.Location = new System.Drawing.Point(3, 47);
+            this.lblCategoryName.Name = "lblCategoryName";
+            this.lblCategoryName.Size = new System.Drawing.Size(116, 32);
+            this.lblCategoryName.TabIndex = 0;
+            this.lblCategoryName.Text = "Category";
+            // 
+            // pnlBackground
+            // 
+            this.pnlBackground.BackColor = System.Drawing.Color.Transparent;
+            this.pnlBackground.BorderRadius = 5;
+            this.pnlBackground.Controls.Add(this.label1);
+            this.pnlBackground.Controls.Add(this.lblCategoryName);
+            this.pnlBackground.FillColor = System.Drawing.Color.MediumAquamarine;
+            this.pnlBackground.Location = new System.Drawing.Point(3, 3);
+            this.pnlBackground.Name = "pnlBackground";
+            this.pnlBackground.Size = new System.Drawing.Size(127, 109);
+            this.pnlBackground.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "13 Items";
+            // 
+            // ucCategoryCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.pnlBackground);
+            this.Name = "ucCategoryCard";
+            this.Size = new System.Drawing.Size(133, 115);
+            this.pnlBackground.ResumeLayout(false);
+            this.pnlBackground.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCategoryName;
+        private Guna.UI2.WinForms.Guna2Panel pnlBackground;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ThaiTanic/UserControls/ucCategoryCard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThaiTanic.UserControls
+{
+    public partial class ucCategoryCard : UserControl
+    {
+        public ucCategoryCard()
+        {
+            InitializeComponent();
+        }
+
+        private Color _color;
+
+        public Color BgColor
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+                pnlBackground.FillColor = _color;
+            }
+        }
+    }
+}
