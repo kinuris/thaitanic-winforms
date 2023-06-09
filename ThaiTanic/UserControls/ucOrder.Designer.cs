d55cb134a69c4e3a8c83bec298b76c3ec70a517f
Rvondc <rogivon.delacruz@gmail.com>
2023-06-09 22:22:26 +0800

    feat: Add order user control

---
+++ ThaiTanic/UserControls/ucOrder.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace ThaiTanic.UserControls
+{
+    partial class ucOrder
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.pnlOrderStatus = new Guna.UI2.WinForms.Guna2Panel();
+            this.lblOrderStatus = new System.Windows.Forms.Label();
+            this.lblTotalCost = new System.Windows.Forms.Label();
+            this.lblOrderDate = new System.Windows.Forms.Label();
+            this.lblItemCount = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblOrderNumber = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel1.SuspendLayout();
+            this.pnlOrderStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BorderColor = System.Drawing.Color.White;
+            this.guna2Panel1.BorderRadius = 10;
+            this.guna2Panel1.BorderThickness = 1;
+            this.guna2Panel1.Controls.Add(this.pnlOrderStatus);
+            this.guna2Panel1.Controls.Add(this.lblTotalCost);
+            this.guna2Panel1.Controls.Add(this.lblOrderDate);
+            this.guna2Panel1.Controls.Add(this.lblItemCount);
+            this.guna2Panel1.Controls.Add(this.label2);
+            this.guna2Panel1.Controls.Add(this.lblOrderNumber);
+            this.guna2Panel1.Controls.Add(this.label1);
+            this.guna2Panel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(59)))));
+            this.guna2Panel1.Location = new System.Drawing.Point(3, 3);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(736, 86);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // pnlOrderStatus
+            // 
+            this.pnlOrderStatus.BackColor = System.Drawing.Color.Transparent;
+            this.pnlOrderStatus.BorderRadius = 5;
+            this.pnlOrderStatus.Controls.Add(this.lblOrderStatus);
+            this.pnlOrderStatus.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(162)))), ((int)(((byte)(78)))));
+            this.pnlOrderStatus.Location = new System.Drawing.Point(600, 43);
+            this.pnlOrderStatus.Name = "pnlOrderStatus";
+            this.pnlOrderStatus.Size = new System.Drawing.Size(110, 32);
+            this.pnlOrderStatus.TabIndex = 6;
+            // 
+            // lblOrderStatus
+            // 
+            this.lblOrderStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblOrderStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrderStatus.ForeColor = System.Drawing.Color.White;
+            this.lblOrderStatus.Location = new System.Drawing.Point(0, 0);
+            this.lblOrderStatus.Name = "lblOrderStatus";
+            this.lblOrderStatus.Size = new System.Drawing.Size(110, 32);
+            this.lblOrderStatus.TabIndex = 7;
+            this.lblOrderStatus.Text = "To Receive";
+            this.lblOrderStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotalCost
+            // 
+            this.lblTotalCost.AutoSize = true;
+            this.lblTotalCost.BackColor = System.Drawing.Color.Transparent;
+            this.lblTotalCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalCost.ForeColor = System.Drawing.Color.White;
+            this.lblTotalCost.Location = new System.Drawing.Point(609, 11);
+            this.lblTotalCost.Name = "lblTotalCost";
+            this.lblTotalCost.Size = new System.Drawing.Size(90, 29);
+            this.lblTotalCost.TabIndex = 5;
+            this.lblTotalCost.Text = "369.69";
+            this.lblTotalCost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOrderDate
+            // 
+            this.lblOrderDate.AutoSize = true;
+            this.lblOrderDate.BackColor = System.Drawing.Color.Transparent;
+            this.lblOrderDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrderDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(117)))), ((int)(((byte)(117)))));
+            this.lblOrderDate.Location = new System.Drawing.Point(26, 60);
+            this.lblOrderDate.Name = "lblOrderDate";
+            this.lblOrderDate.Size = new System.Drawing.Size(154, 17);
+            this.lblOrderDate.TabIndex = 4;
+            this.lblOrderDate.Text = "June 3, 2023 11:28 am";
+            // 
+            // lblItemCount
+            // 
+            this.lblItemCount.AutoSize = true;
+            this.lblItemCount.BackColor = System.Drawing.Color.Transparent;
+            this.lblItemCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItemCount.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(117)))), ((int)(((byte)(117)))));
+            this.lblItemCount.Location = new System.Drawing.Point(122, 41);
+            this.lblItemCount.Name = "lblItemCount";
+            this.lblItemCount.Size = new System.Drawing.Size(16, 17);
+            this.lblItemCount.TabIndex = 3;
+            this.lblItemCount.Text = "4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(117)))), ((int)(((byte)(117)))));
+            this.label2.Location = new System.Drawing.Point(26, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Items ordered:";
+            // 
+            // lblOrderNumber
+            // 
+            this.lblOrderNumber.AutoSize = true;
+            this.lblOrderNumber.BackColor = System.Drawing.Color.Transparent;
+            this.lblOrderNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrderNumber.ForeColor = System.Drawing.Color.White;
+            this.lblOrderNumber.Location = new System.Drawing.Point(108, 9);
+            this.lblOrderNumber.Name = "lblOrderNumber";
+            this.lblOrderNumber.Size = new System.Drawing.Size(65, 24);
+            this.lblOrderNumber.TabIndex = 1;
+            this.lblOrderNumber.Text = "13123";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Order: # ";
+            // 
+            // ucOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.guna2Panel1);
+            this.Name = "ucOrder";
+            this.Size = new System.Drawing.Size(742, 92);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.pnlOrderStatus.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel pnlOrderStatus;
+        private System.Windows.Forms.Label lblOrderStatus;
+        private System.Windows.Forms.Label lblTotalCost;
+        private System.Windows.Forms.Label lblOrderDate;
+        private System.Windows.Forms.Label lblItemCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblOrderNumber;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ThaiTanic/UserControls/ucOrder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThaiTanic.UserControls
+{
+    public partial class ucOrder : UserControl
+    {
+        public ucOrder(int orderNumber, int itemCount, DateTime orderDate, double totalCost, string orderStatus)
+        {
+            InitializeComponent();
+
+            lblOrderNumber.Text = orderNumber.ToString();
+            lblItemCount.Text = itemCount.ToString();
+            lblOrderDate.Text = orderDate.ToString("f");
+            lblTotalCost.Text = totalCost.ToString("₱#,###.00");
+            lblOrderStatus.Text = orderStatus;
+
+            switch (orderStatus)
+            {
+                case "To Pay":
+                    pnlOrderStatus.FillColor = Color.FromArgb(225, 177, 44);
+                    break;
+                case "To Ship":
+                    pnlOrderStatus.FillColor = Color.FromArgb(12, 140, 233);
+                    break;
+                case "To Receive":
+                    pnlOrderStatus.FillColor = Color.FromArgb(75, 162, 78);
+                    break;
+                case "Completed":
+                    pnlOrderStatus.FillColor = Color.FromArgb(147, 109, 212);
+                    break;
+                case "Canceled":
+                    pnlOrderStatus.FillColor = Color.FromArgb(195, 29, 29);
+                    break;
+            }
+        }
+    }
+}
