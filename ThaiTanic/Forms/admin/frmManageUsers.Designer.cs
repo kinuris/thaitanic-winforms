a55c65fb7260460d5b090fd360ec929ac2f5104c
Rvondc <rogivon.delacruz@gmail.com>
2023-06-12 22:43:05 +0800

    feat: Add manage users form UI

---
+++ ThaiTanic/Forms/admin/frmManageUsers.Designer.cs
@@ -0,0 +1,300 @@
+﻿
+namespace ThaiTanic.Forms.admin
+{
+    partial class frmManageUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmManageUsers));
+            this.guna2Panel5 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel6 = new Guna.UI2.WinForms.Guna2Panel();
+            this.cbUsers = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel4 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPermissions = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearchUsers = new Guna.UI2.WinForms.Guna2TextBox();
+            this.cbUserRole = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guna2Panel5.SuspendLayout();
+            this.guna2Panel6.SuspendLayout();
+            this.guna2Panel2.SuspendLayout();
+            this.guna2Panel4.SuspendLayout();
+            this.guna2Panel1.SuspendLayout();
+            this.guna2Panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel5
+            // 
+            this.guna2Panel5.Controls.Add(this.guna2Panel6);
+            this.guna2Panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel5.Location = new System.Drawing.Point(0, 146);
+            this.guna2Panel5.Name = "guna2Panel5";
+            this.guna2Panel5.Size = new System.Drawing.Size(1048, 344);
+            this.guna2Panel5.TabIndex = 7;
+            // 
+            // guna2Panel6
+            // 
+            this.guna2Panel6.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel6.BorderRadius = 10;
+            this.guna2Panel6.Controls.Add(this.cbUserRole);
+            this.guna2Panel6.Controls.Add(this.label3);
+            this.guna2Panel6.Controls.Add(this.cbUsers);
+            this.guna2Panel6.Controls.Add(this.label8);
+            this.guna2Panel6.Controls.Add(this.txtSearchUsers);
+            this.guna2Panel6.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(59)))));
+            this.guna2Panel6.Location = new System.Drawing.Point(30, 3);
+            this.guna2Panel6.Name = "guna2Panel6";
+            this.guna2Panel6.Size = new System.Drawing.Size(988, 336);
+            this.guna2Panel6.TabIndex = 11;
+            // 
+            // cbUsers
+            // 
+            this.cbUsers.BackColor = System.Drawing.Color.Transparent;
+            this.cbUsers.BorderRadius = 10;
+            this.cbUsers.BorderThickness = 0;
+            this.cbUsers.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbUsers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbUsers.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(31)))), ((int)(((byte)(44)))));
+            this.cbUsers.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbUsers.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbUsers.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cbUsers.ForeColor = System.Drawing.Color.White;
+            this.cbUsers.ItemHeight = 30;
+            this.cbUsers.ItemsAppearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(31)))), ((int)(((byte)(44)))));
+            this.cbUsers.ItemsAppearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.cbUsers.ItemsAppearance.SelectedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(31)))), ((int)(((byte)(44)))));
+            this.cbUsers.ItemsAppearance.SelectedForeColor = System.Drawing.Color.White;
+            this.cbUsers.Location = new System.Drawing.Point(35, 103);
+            this.cbUsers.Name = "cbUsers";
+            this.cbUsers.Size = new System.Drawing.Size(325, 36);
+            this.cbUsers.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.label8.Location = new System.Drawing.Point(34, 84);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 18);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Users:";
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.Controls.Add(this.guna2Panel4);
+            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel2.Location = new System.Drawing.Point(0, 66);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(1048, 80);
+            this.guna2Panel2.TabIndex = 6;
+            // 
+            // guna2Panel4
+            // 
+            this.guna2Panel4.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel4.BorderRadius = 10;
+            this.guna2Panel4.Controls.Add(this.label2);
+            this.guna2Panel4.Controls.Add(this.btnPermissions);
+            this.guna2Panel4.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(59)))));
+            this.guna2Panel4.Location = new System.Drawing.Point(30, 3);
+            this.guna2Panel4.Name = "guna2Panel4";
+            this.guna2Panel4.Size = new System.Drawing.Size(988, 75);
+            this.guna2Panel4.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(30, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Manage Users";
+            // 
+            // btnPermissions
+            // 
+            this.btnPermissions.BorderColor = System.Drawing.Color.Transparent;
+            this.btnPermissions.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnPermissions.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnPermissions.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnPermissions.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnPermissions.FillColor = System.Drawing.Color.Transparent;
+            this.btnPermissions.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnPermissions.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.btnPermissions.HoverState.BorderColor = System.Drawing.Color.Transparent;
+            this.btnPermissions.HoverState.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.btnPermissions.HoverState.FillColor = System.Drawing.Color.Transparent;
+            this.btnPermissions.HoverState.ForeColor = System.Drawing.Color.White;
+            this.btnPermissions.Location = new System.Drawing.Point(22, 36);
+            this.btnPermissions.Name = "btnPermissions";
+            this.btnPermissions.PressedColor = System.Drawing.Color.Transparent;
+            this.btnPermissions.Size = new System.Drawing.Size(115, 26);
+            this.btnPermissions.TabIndex = 0;
+            this.btnPermissions.Text = "Permissions";
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Controls.Add(this.guna2Panel3);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(1048, 66);
+            this.guna2Panel1.TabIndex = 5;
+            // 
+            // guna2Panel3
+            // 
+            this.guna2Panel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel3.BorderRadius = 10;
+            this.guna2Panel3.Controls.Add(this.label1);
+            this.guna2Panel3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(59)))));
+            this.guna2Panel3.Location = new System.Drawing.Point(30, 18);
+            this.guna2Panel3.Name = "guna2Panel3";
+            this.guna2Panel3.Size = new System.Drawing.Size(988, 45);
+            this.guna2Panel3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(336, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thai-Tanic Admin Page";
+            // 
+            // txtSearchUsers
+            // 
+            this.txtSearchUsers.BackColor = System.Drawing.Color.Transparent;
+            this.txtSearchUsers.BorderRadius = 10;
+            this.txtSearchUsers.BorderThickness = 0;
+            this.txtSearchUsers.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSearchUsers.DefaultText = "";
+            this.txtSearchUsers.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtSearchUsers.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtSearchUsers.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtSearchUsers.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtSearchUsers.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(31)))), ((int)(((byte)(44)))));
+            this.txtSearchUsers.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtSearchUsers.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtSearchUsers.ForeColor = System.Drawing.Color.White;
+            this.txtSearchUsers.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtSearchUsers.IconLeft = ((System.Drawing.Image)(resources.GetObject("txtSearchUsers.IconLeft")));
+            this.txtSearchUsers.IconLeftOffset = new System.Drawing.Point(5, 0);
+            this.txtSearchUsers.IconLeftSize = new System.Drawing.Size(28, 28);
+            this.txtSearchUsers.Location = new System.Drawing.Point(35, 9);
+            this.txtSearchUsers.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSearchUsers.Name = "txtSearchUsers";
+            this.txtSearchUsers.PasswordChar = '\0';
+            this.txtSearchUsers.PlaceholderText = "Look for the user here.";
+            this.txtSearchUsers.SelectedText = "";
+            this.txtSearchUsers.Size = new System.Drawing.Size(555, 36);
+            this.txtSearchUsers.TabIndex = 1;
+            this.txtSearchUsers.TextOffset = new System.Drawing.Point(5, 0);
+            // 
+            // cbUserRole
+            // 
+            this.cbUserRole.BackColor = System.Drawing.Color.Transparent;
+            this.cbUserRole.BorderRadius = 10;
+            this.cbUserRole.BorderThickness = 0;
+            this.cbUserRole.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbUserRole.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbUserRole.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(31)))), ((int)(((byte)(44)))));
+            this.cbUserRole.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbUserRole.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbUserRole.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cbUserRole.ForeColor = System.Drawing.Color.White;
+            this.cbUserRole.ItemHeight = 30;
+            this.cbUserRole.ItemsAppearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(31)))), ((int)(((byte)(44)))));
+            this.cbUserRole.ItemsAppearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.cbUserRole.ItemsAppearance.SelectedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(31)))), ((int)(((byte)(44)))));
+            this.cbUserRole.ItemsAppearance.SelectedForeColor = System.Drawing.Color.White;
+            this.cbUserRole.Location = new System.Drawing.Point(37, 184);
+            this.cbUserRole.Name = "cbUserRole";
+            this.cbUserRole.Size = new System.Drawing.Size(325, 36);
+            this.cbUserRole.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.label3.Location = new System.Drawing.Point(36, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 18);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "User Role:";
+            // 
+            // frmManageUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(31)))), ((int)(((byte)(44)))));
+            this.ClientSize = new System.Drawing.Size(1048, 720);
+            this.Controls.Add(this.guna2Panel5);
+            this.Controls.Add(this.guna2Panel2);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmManageUsers";
+            this.Text = "frmManageUsers";
+            this.guna2Panel5.ResumeLayout(false);
+            this.guna2Panel6.ResumeLayout(false);
+            this.guna2Panel6.PerformLayout();
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel4.ResumeLayout(false);
+            this.guna2Panel4.PerformLayout();
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel3.ResumeLayout(false);
+            this.guna2Panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel5;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel6;
+        private Guna.UI2.WinForms.Guna2ComboBox cbUsers;
+        private System.Windows.Forms.Label label8;
+        private Guna.UI2.WinForms.Guna2TextBox txtSearchUsers;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel4;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2Button btnPermissions;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2ComboBox cbUserRole;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ ThaiTanic/Forms/admin/frmManageUsers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThaiTanic.Forms.admin
+{
+    public partial class frmManageUsers : Form
+    {
+        public frmManageUsers()
+        {
+            InitializeComponent();
+
+            string[] role = new string[] { "user", "admin" };
+            cbUserRole.Items.AddRange(role);
+        }
+    }
+}
