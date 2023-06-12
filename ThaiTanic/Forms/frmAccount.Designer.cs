c92241fc90268dd4b0e89e40c67385a6673e0a0c
Rvondc <rogivon.delacruz@gmail.com>
2023-06-12 22:43:43 +0800

    feat: Add account form UI

---
+++ ThaiTanic/Forms/frmAccount.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace ThaiTanic.Forms
+{
+    partial class frmAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlContainer = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lblAccountLabel = new System.Windows.Forms.Label();
+            this.btnProfile = new Guna.UI2.WinForms.Guna2Button();
+            this.btnBillingAddress = new Guna.UI2.WinForms.Guna2Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.guna2Panel4 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel2.SuspendLayout();
+            this.guna2Panel1.SuspendLayout();
+            this.guna2Panel4.SuspendLayout();
+            this.guna2Panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlContainer
+            // 
+            this.pnlContainer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlContainer.Location = new System.Drawing.Point(0, 146);
+            this.pnlContainer.Name = "pnlContainer";
+            this.pnlContainer.Padding = new System.Windows.Forms.Padding(30, 5, 30, 0);
+            this.pnlContainer.Size = new System.Drawing.Size(1134, 529);
+            this.pnlContainer.TabIndex = 8;
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.Controls.Add(this.guna2Panel4);
+            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel2.Location = new System.Drawing.Point(0, 66);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(1134, 80);
+            this.guna2Panel2.TabIndex = 3;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Controls.Add(this.guna2Panel3);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(1134, 66);
+            this.guna2Panel1.TabIndex = 2;
+            // 
+            // lblAccountLabel
+            // 
+            this.lblAccountLabel.AutoSize = true;
+            this.lblAccountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAccountLabel.ForeColor = System.Drawing.Color.White;
+            this.lblAccountLabel.Location = new System.Drawing.Point(30, 13);
+            this.lblAccountLabel.Name = "lblAccountLabel";
+            this.lblAccountLabel.Size = new System.Drawing.Size(142, 25);
+            this.lblAccountLabel.TabIndex = 0;
+            this.lblAccountLabel.Text = "User Account";
+            // 
+            // btnProfile
+            // 
+            this.btnProfile.BorderColor = System.Drawing.Color.Transparent;
+            this.btnProfile.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnProfile.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnProfile.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnProfile.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnProfile.FillColor = System.Drawing.Color.Transparent;
+            this.btnProfile.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnProfile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.btnProfile.HoverState.BorderColor = System.Drawing.Color.Transparent;
+            this.btnProfile.HoverState.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.btnProfile.HoverState.FillColor = System.Drawing.Color.Transparent;
+            this.btnProfile.HoverState.ForeColor = System.Drawing.Color.White;
+            this.btnProfile.Location = new System.Drawing.Point(18, 36);
+            this.btnProfile.Name = "btnProfile";
+            this.btnProfile.PressedColor = System.Drawing.Color.Transparent;
+            this.btnProfile.Size = new System.Drawing.Size(85, 26);
+            this.btnProfile.TabIndex = 3;
+            this.btnProfile.Text = "Profile";
+            this.btnProfile.Click += new System.EventHandler(this.btnProfile_Click);
+            // 
+            // btnBillingAddress
+            // 
+            this.btnBillingAddress.BorderColor = System.Drawing.Color.Transparent;
+            this.btnBillingAddress.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnBillingAddress.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnBillingAddress.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnBillingAddress.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnBillingAddress.FillColor = System.Drawing.Color.Transparent;
+            this.btnBillingAddress.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnBillingAddress.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.btnBillingAddress.HoverState.BorderColor = System.Drawing.Color.Transparent;
+            this.btnBillingAddress.HoverState.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.btnBillingAddress.HoverState.FillColor = System.Drawing.Color.Transparent;
+            this.btnBillingAddress.HoverState.ForeColor = System.Drawing.Color.White;
+            this.btnBillingAddress.Location = new System.Drawing.Point(116, 36);
+            this.btnBillingAddress.Name = "btnBillingAddress";
+            this.btnBillingAddress.PressedColor = System.Drawing.Color.Transparent;
+            this.btnBillingAddress.Size = new System.Drawing.Size(137, 26);
+            this.btnBillingAddress.TabIndex = 4;
+            this.btnBillingAddress.Text = "Billing Address";
+            this.btnBillingAddress.Click += new System.EventHandler(this.btnBillingAddress_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.label4.Location = new System.Drawing.Point(103, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "/";
+            // 
+            // guna2Panel4
+            // 
+            this.guna2Panel4.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel4.BorderRadius = 10;
+            this.guna2Panel4.Controls.Add(this.label4);
+            this.guna2Panel4.Controls.Add(this.btnBillingAddress);
+            this.guna2Panel4.Controls.Add(this.btnProfile);
+            this.guna2Panel4.Controls.Add(this.lblAccountLabel);
+            this.guna2Panel4.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(59)))));
+            this.guna2Panel4.Location = new System.Drawing.Point(30, 3);
+            this.guna2Panel4.Name = "guna2Panel4";
+            this.guna2Panel4.Size = new System.Drawing.Size(1074, 75);
+            this.guna2Panel4.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(477, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Account";
+            // 
+            // guna2Panel3
+            // 
+            this.guna2Panel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel3.BorderRadius = 10;
+            this.guna2Panel3.Controls.Add(this.label1);
+            this.guna2Panel3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(59)))));
+            this.guna2Panel3.Location = new System.Drawing.Point(30, 18);
+            this.guna2Panel3.Name = "guna2Panel3";
+            this.guna2Panel3.Size = new System.Drawing.Size(1074, 45);
+            this.guna2Panel3.TabIndex = 0;
+            // 
+            // frmAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(31)))), ((int)(((byte)(44)))));
+            this.ClientSize = new System.Drawing.Size(1134, 768);
+            this.Controls.Add(this.pnlContainer);
+            this.Controls.Add(this.guna2Panel2);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmAccount";
+            this.Text = "frmAccount";
+            this.Load += new System.EventHandler(this.frmAccount_Load);
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel4.ResumeLayout(false);
+            this.guna2Panel4.PerformLayout();
+            this.guna2Panel3.ResumeLayout(false);
+            this.guna2Panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2Panel pnlContainer;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel4;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2Button btnBillingAddress;
+        private Guna.UI2.WinForms.Guna2Button btnProfile;
+        private System.Windows.Forms.Label lblAccountLabel;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ThaiTanic/Forms/frmAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThaiTanic.Forms
+{
+    public partial class frmAccount : Form
+    {
+        public frmAccount()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAccount_Load(object sender, EventArgs e)
+        {
+            frmAccountProfile frmAccountProfile = new frmAccountProfile();
+            frmAccountProfile.TopLevel = false;
+            frmAccountProfile.Dock = DockStyle.Fill;
+            pnlContainer.Controls.Add(frmAccountProfile);
+            frmAccountProfile.Show();
+        }
+
+        private void btnProfile_Click(object sender, EventArgs e)
+        {
+            pnlContainer.Controls.Clear();
+
+            frmAccountProfile frmAccountProfile = new frmAccountProfile();
+            frmAccountProfile.TopLevel = false;
+            frmAccountProfile.Dock = DockStyle.Fill;
+            pnlContainer.Controls.Add(frmAccountProfile);
+            frmAccountProfile.Show();
+
+            lblAccountLabel.Text = "User Account";
+        }
+
+        private void btnBillingAddress_Click(object sender, EventArgs e)
+        {
+            pnlContainer.Controls.Clear();
+
+            frmAccountBillingAddress frmAccountBillingAddress = new frmAccountBillingAddress();
+            frmAccountBillingAddress.TopLevel = false;
+            frmAccountBillingAddress.Dock = DockStyle.Fill;
+            pnlContainer.Controls.Add(frmAccountBillingAddress);
+            frmAccountBillingAddress.Show();
+
+            lblAccountLabel.Text = "Billing Address";
+        }
+    }
+}
