ac79f73446f03939cf57a5cd1b54129eff890411
chanc <chanchrisdominic@gmail.com>
2023-07-06 08:42:52 +0800

    Fixed possible index out of bounds access

--- ThaiTanic/Forms/frmMenu.cs
+++ ThaiTanic/Forms/frmMenu.cs
@@ -76,7 +76,7 @@ namespace ThaiTanic.Forms
 
                 ucCategoryCard categoryCard = new ucCategoryCard(category.DBString(), Items.GetAllItems().Where(e => e.Category == category).Where(entry => entry.Available).Count(), SetCurrentCategory)
                 {
-                    BgColor = colors[iteration++ % 9]
+                    BgColor = colors[iteration++ % colors.Count()]
                 };
 
                 pnlContainerCategories.Controls.Add(categoryCard);
